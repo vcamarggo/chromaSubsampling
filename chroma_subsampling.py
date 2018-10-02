e66e3221f29b150c6a6b642ab6fdac762871133a
Igor Santana <igorsantana@Igors-MacBook-Pro.local>
2018-10-02 17:11:39 -0300

    ycbcr chroma subsampling

--- chroma_subsampling.py
+++ chroma_subsampling.py
@@ -1,66 +1,25 @@
 import numpy as np
 import cv2
 
-GRAYSCALE=0
-RGB = 1
+mode = { '4:4:4': [1, 1],'4:4:0': [1,2], '4:2:2': [2,1],  '4:2:0': [2,2], '4:1:1': [4,1]}
 
-Y=0
-Cr=1
-Cb=2
+subsampling = lambda img, mode : img[::mode[0], ::mode[1]]
+upsampling	= lambda img, mode : np.repeat(np.repeat(img, mode[0], axis=0), mode[1], axis=1)
 
-ROW=0
-COLUMN=1
+ycbcr_img = cv2.cvtColor(cv2.imread('stairs.jpg', 1) , cv2.COLOR_BGR2YCrCb)
+y, cr, cb = cv2.split(ycbcr_img)
 
-IN=0
-OUT=1
+for key, value in mode.items():
+	cb_sub = subsampling(cb, value)
+	cr_sub = subsampling(cr, value)
 
-#4:4:4
-MODE_4_4_4 = [1,1]
-#4:1:1
-MODE_4_1_1 = [4,1]
-#4:2:2
-MODE_4_2_2 = [2,1]
-#4:2:0
-MODE_4_2_2 = [2,2]
+	print('[' + key +'] Pre  sample: ' + str(y.size + cb.size + cr.size))
+	print('[' + key +'] Post sample: ' + str(y.size + cb_sub.size + cr_sub.size))
+	print('')
+	cb_unsub = upsampling(cb_sub, value)
+	cr_unsub = upsampling(cr_sub, value)
 
-#COLOR_SYSTEM
-LUV = [cv2.COLOR_BGR2Luv,cv2.COLOR_Luv2BGR] 
-YCrCb = [cv2.COLOR_BGR2YCrCb,cv2.COLOR_YCrCb2BGR] 
-YUV = [cv2.COLOR_BGR2YUV,cv2.COLOR_YUV2BGR] 
+	result 	= cv2.merge((y, cr_unsub, cb_unsub))
+	bgr_out = cv2.cvtColor(result, cv2.COLOR_YCrCb2BGR)
 
-def apply_chroma_subsampling(img, MODE):
-	return img[::MODE[0], ::MODE[1]]
-	
-def unapply_chroma_subsampling(img, MODE):
-	rowDecompressed = np.repeat(img, MODE[0], axis=ROW)
-	return np.repeat(rowDecompressed, MODE[1], axis=COLUMN)
-
-#Implementar cálculo de multiplo de 4, senao nao consegue fazer os quadrado do calculo
-imgBGR = cv2.resize(cv2.imread('colors.jpg',RGB),(1680, 1052))
-
-print("Original %d" %(imgBGR.size))
-
-#Implementar entrada de dados
-chromaMode = MODE_4_1_1
-colorSystem = LUV
-
-imgYCrCb = cv2.cvtColor(imgBGR, colorSystem[IN])
-
-(imageY,imageCr,imageCb) = cv2.split(imgYCrCb)
-
-subsampleCb = apply_chroma_subsampling(imageCb, chromaMode)
-subsampleCr = apply_chroma_subsampling(imageCr, chromaMode)
-
-print("Aplicado subsampling: %d" %(imageY.size + subsampleCb.size + subsampleCr.size))
-
-unsampledCr = unapply_chroma_subsampling(subsampleCr, chromaMode)
-unsampledCb = unapply_chroma_subsampling(subsampleCb, chromaMode)
-
-result = cv2.merge((imageY, unsampledCr, unsampledCb))
-
-compressedBGR = cv2.cvtColor(result, colorSystem[OUT])
-
-print("Novo RGB %d" %(compressedBGR.size))
-
-# cv2.imshow('image', compressedBRG) #remover, apenas mostra a imagem
-#cv2.imwrite('colors_subsampled.jpg',compressedBGR) #fazer um esquema que busque o nome da entrada e de append 'subsampled'
+	cv2.imwrite('stairs_subsample_' + str(key) +'.jpg', bgr_out) 
